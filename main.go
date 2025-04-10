91736e23e83155c07ea30ab8dc522f8c555d5038
rahimuj570 <rahimujjaman2002@gmail.com>
2025-04-10 22:14:03 +0600

    create todo api work. store todo in map as practice

--- main.go
+++ main.go
@@ -1,19 +1,60 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"sync"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /test", test)
+	mux.HandleFunc("GET /", test)
+	mux.HandleFunc("POST /add", createTodo)
+
 	fmt.Println("Hello API")
 
 	//Listening
-	http.ListenAndServe(":8000", mux)
+	log.Fatal((http.ListenAndServe(":8000", mux)))
 }
 
+// struct for TODO
+type TODO struct {
+	Title string `json:"title"`
+	Do    string `json:"do"`
+}
+
+// todo cache. local temp db for practice
+var todoCache = make(map[int]TODO)
+
+// to test the server
 func test(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "API WORKING")
+}
+
+// for limit access
+var mutex sync.RWMutex
+
+// to post todo
+func createTodo(w http.ResponseWriter, r *http.Request) {
+	var todo TODO
+	err := json.NewDecoder(r.Body).Decode(&todo)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if todo.Do == "" || todo.Title == "" {
+		http.Error(w, "invalid input", http.StatusBadRequest)
+		return
+	}
+
+	mutex.Lock()
+	todoCache[len(todoCache)+1] = todo
+	mutex.Unlock()
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Println(todoCache)
 }
